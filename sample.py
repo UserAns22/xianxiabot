507807b84c813b7d3d154ebf579318bfca602732
UserAns22 <aniapplefan@gmail.com>
2017-11-14 07:35:32 -0800

    Implemented Checkpoints

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ import os
 from six.moves import cPickle
 
 import rnn
-from train import SAVE_DIR
+SAVE_DIR = './output'
 
 num_chars = 500
 prime = 'M' #start with Meng Hao!
@@ -12,16 +12,15 @@ sample = 1 #
 
 def sample():
     with open(os.path.join(SAVE_DIR, 'conf.pkl'), 'rb') as f:
-        size, chars, vocab = cPickle.load(f)
+        vocab_size, chars, vocab = cPickle.load(f)
     rnn.training = False
-    model = rnn.Model(size)
+    model = rnn.Model(vocab_size)
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
         saver = tf.train.Saver(tf.global_variables())
-        ckpt = tf.train.get_checkpoint_state(SAVE_DIR)
-        if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, chars, vocab, num_chars,sample).encode('utf-8')) 
+        ckpt = os.path.join(SAVE_DIR, 'model.ckpt-109545')
+        saver.restore(sess, ckpt)
+        print(model.sample(sess, chars, vocab, num_chars,sample).encode('utf-8')) 
 
 if __name__ == "__main__":
     sample()
--- train.py
+++ train.py
@@ -7,10 +7,10 @@ from six.moves import cPickle
 from utils import TextLoader
 import rnn
 
-NUM_EPOCHS = 50
+NUM_EPOCHS = 1
 DATA_DIR = './data'
 SAVE_DIR = './output'
-init_from = None #SAVE_DIR
+init_from = SAVE_DIR
 
 
 def train():
@@ -21,7 +21,7 @@ def train():
         ckpt = tf.train.get_checkpoint_state(init_from)
         
     with open(os.path.join(SAVE_DIR + 'conf.pkl'), 'wb') as f:
-        cPickle.dump((loader.vocab_size, loader.chars, loader.vocab))
+        cPickle.dump((loader.vocab_size, loader.chars, loader.vocab), f)
 
     model = rnn.Model(vocab_size)
 
@@ -46,8 +46,9 @@ def train():
                 
                 train_loss, state, _= sess.run([model.cost, model.final_state, model.train_op], feed)
                 end = time.time()
-                print('{}/{} (epoch {}), train_loss = {}, time/batch = {}'.format(
-                    curr_batch, NUM_EPOCHS * loader.num_batches, i, train_loss, end - start))
+                print('{}/{} (epoch {}), train_loss = {}, time/batch = {}, time_left = {}'.format(
+                    curr_batch, NUM_EPOCHS * loader.num_batches, i, train_loss, end - start,
+                    ((end - start) * (NUM_EPOCHS * loader.num_batches - curr_batch))))
                 if curr_batch % 1000 == 0 or ( i == (NUM_EPOCHS - 1) and (b == loader.num_batches - 1)):
                     ckpath = os.path.join(SAVE_DIR, 'model.ckpt')
                     saver.save(sess, ckpath, global_step = curr_batch)
